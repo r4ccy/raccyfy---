0f6cdf8bf6666aa05141a7fde22277bc9eeab989
Adriana <adrianasotojimenez3@gmail.com>
2025-07-04 21:04:28 -0400

    feat: actualizar componente home para invitado

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,14 @@
 import "./Home.css";
+import useUserStore from "../store/useUserStore.js"; // Importar el store de Zustand
 
 function Home() {
+    const {guestName, token} = useUserStore();
+
     return (
         <div className="home">
-            <h1>holiii</h1>
+            <h1>
+                holiii {guestName ? `(${guestName})` : token ? "(Spotify)" : ""}
+            </h1>
             <p>prueba 1</p>
         </div>
     );
